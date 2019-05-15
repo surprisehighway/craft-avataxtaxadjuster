4bbdf6af067385c1153980bf3bcb640a0cad77cc
jmauzyk <jmauzyk@gmail.com>
2019-05-15 10:01:14 -0500

    Require shipping method to fetch tax rates

--- adjusters/AvataxTaxAdjuster.php
+++ adjusters/AvataxTaxAdjuster.php
@@ -11,7 +11,7 @@ class AvataxTaxAdjuster implements Commerce_AdjusterInterface {
 
   public function adjust(Commerce_OrderModel &$order, array $lineItems = []){
 
-    if( $order->shippingAddress !== NULL && sizeof($order->lineItems) > 0)
+    if ($order->shippingAddress !== null && $order->shippingMethod != null && sizeof($order->lineItems) > 0)
     {
       $taxService = new SalesTaxService;
 
